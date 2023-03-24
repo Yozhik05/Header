3c3e2f1acd31384b830cd05abb25f8008b16568d
Yozhik05 <minecraftdag@yandex.ru>
2023-03-24 18:31:12 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="item item_1">Home</div>{" "}
+        <div className="item item_3">Service</div>
+        <div className="item item_4">Contact</div>{" "}
+        <div className="item item_2">About</div>
+      </div>
+
+      {/* <a href="#" className="a a1"></a>
+      <a href="#" className="a a2"></a>
+      <a href="#" className="a a3"></a>
+      <a href="#" className="a a4"></a> */}
+    </div>
+  );
+}
